440437ec681ca6ae769b73b9b4d75f70244b41bd
Shridhartadakod <shridhar1491998@gmail.com>
2022-03-10 18:20:37 +0530

    Avoid Dplication

---
+++ AddressBookSystem/AddressBookLibrary.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class AddressBookLibrary
+    {
+        private readonly Dictionary<string, AddressBook> library;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AddressBookLibrary"/> class.
+        /// </summary>
+        public AddressBookLibrary()
+        {
+            library = new Dictionary<string, AddressBook>();
+        }
+
+        /// <summary>
+        /// Adds an address book to the library.
+        /// </summary>
+        public void AddAddressBook()
+        {
+            string name = UserInput.GetName("Enter Name of AddressBook: ");
+            if (library.ContainsKey(name) is false && String.IsNullOrEmpty(name) is false)
+            {
+                library.Add(name, new AddressBook());
+                Console.WriteLine("AddressBook Added Successfully");
+            }
+            else if (String.IsNullOrEmpty(name))
+                Console.WriteLine("Invalid name");
+            else
+                Console.WriteLine("AddressBook with that name already exists");
+        }
+
+        /// <summary>
+        /// Opens an existing AddressBook with menu option to interact with it
+        /// </summary>
+        public void OpenAddressBook()
+        {
+            string name = UserInput.GetName("Enter Name of AddressBook: ");
+            if (library.ContainsKey(name))
+                AddressBookMenu.List(name, library[name]);
+            else
+                Console.WriteLine("Addressbook with that name does not exist");
+        }
+
+        /// <summary>
+        /// Display all Address Books in the library
+        /// </summary>
+        public void Display()
+        {
+            Console.WriteLine("List of Address Books:");
+            foreach (var name in library.Keys)
+                Console.WriteLine(name);
+        }
+    }
+}
---
+++ AddressBookSystem/LibraryMenu.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class LibraryMenu
+    {
+        public static void List()
+        {
+            int option;
+            AddressBookLibrary mylibrary = new AddressBookLibrary();
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("-------------Address Books Library-------------");
+                Console.WriteLine("Choose from following:\n");
+                Console.WriteLine("1. Create New Address Book");
+                Console.WriteLine("2. Open an AddressBook");
+                Console.WriteLine("3. Display all Address Books in library");
+                Console.WriteLine("4. Exit");
+                option = Userinput.GetPositiveInt("Enter option(1-4): ");
+                Console.Clear();
+                switch (option)
+                {
+                    case 1:
+                        mylibrary.AddAddressBook();
+                        break;
+                    case 2:
+                        mylibrary.OpenAddressBook();
+                        break;
+                    case 3:
+                        mylibrary.Display();
+                        break;
+                    case 4:
+                        Console.WriteLine("Exiting Application...");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Option!!!");
+                        break;
+                }
+                if (option == 4)
+                    break;
+                Console.WriteLine("Press any key to Continue...");
+                Console.ReadKey();
+            } while (option != 4);
+        }
+    }
+}
